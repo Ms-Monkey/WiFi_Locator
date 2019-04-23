100dca370db540f48f10e4ac68cb05b2cc27718d
Ms-Monkey <38368644+Ms-Monkey@users.noreply.github.com>
2019-04-24 10:22:22 +1200

    Create Router.java

---
+++ Router.java
@@ -0,0 +1,15 @@
+package com.example.sorensjone.test;
+
+public class Router {
+    public int x, y, z;
+    public String SSID, BSSID;
+
+    public Router(int x, int y, int z, String SSID, String BSSID){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.SSID = SSID;
+        this.BSSID = BSSID;
+    }
+
+}
