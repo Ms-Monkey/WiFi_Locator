22138c1fc659733d2cdad4a33bbd93404ce5f352
Ms-Monkey <38368644+Ms-Monkey@users.noreply.github.com>
2019-04-24 12:58:55 +1200

    Displays distance in table

--- DisplayMessageActivity.java
+++ DisplayMessageActivity.java
@@ -62,47 +62,47 @@ public class DisplayMessageActivity extends AppCompatActivity {
         TextView textView1 = findViewById(R.id.row1Name);
         textView1.setText(one.SSID);
         TextView textView2 = findViewById(R.id.row1MAC);
-        textView2.setText("" + one.BSSID + " / " + one.frequency);
+        textView2.setText(one.BSSID);
         TextView textView3 = findViewById(R.id.row1Strength);
-        textView3.setText("" + one.level);
+        textView3.setText(one.distance);
 
         WifiSignal two = signals.get(1);
         TextView textView4 = findViewById(R.id.row2Name);
         textView4.setText(two.SSID);
         TextView textView5 = findViewById(R.id.row2MAC);
-        textView5.setText("" + two.BSSID + " / " + two.frequency);
+        textView5.setText(two.BSSID);
         TextView textView6 = findViewById(R.id.row2Strength);
-        textView6.setText("" + two.level);
+        textView6.setText(two.distance);
 
         WifiSignal three = signals.get(2);
         TextView textView7 = findViewById(R.id.row3Name);
         textView7.setText(three.SSID);
         TextView textView8 = findViewById(R.id.row3MAC);
-        textView8.setText("" + three.BSSID + " / " + three.frequency);
+        textView8.setText(three.BSSID);
         TextView textView9 = findViewById(R.id.row3Strength);
-        textView9.setText("" + three.level);
+        textView9.setText(three.distance);
 
         WifiSignal four = signals.get(3);
         TextView textView10 = findViewById(R.id.row4Name);
         textView10.setText(four.SSID);
         TextView textView11 = findViewById(R.id.row4MAC);
-        textView11.setText("" + four.BSSID + " / " + four.frequency);
+        textView11.setText(four.BSSID);
         TextView textView12 = findViewById(R.id.row4Strength);
-        textView12.setText("" + four.level);
+        textView12.setText(four.distance);
 
         WifiSignal five = signals.get(4);
         TextView textView13 = findViewById(R.id.row5Name);
         textView13.setText(five.SSID);
         TextView textView14 = findViewById(R.id.row5MAC);
-        textView14.setText("" + five.BSSID + " / " + five.frequency);
+        textView14.setText(five.BSSID);
         TextView textView15 = findViewById(R.id.row5Strength);
-        textView15.setText("" + five.level);
+        textView15.setText(five.distance);
 
-        WifiSignal six = signals.get(5);
+        /*WifiSignal six = signals.get(5);
         TextView textView16 = findViewById(R.id.row6Name);
         textView16.setText(six.SSID);
         TextView textView17 = findViewById(R.id.row6MAC);
-        textView17.setText("" + six.BSSID + " / " + six.frequency);
+        textView17.setText("" + six.BSSID + " / " + six.distance);
         TextView textView18 = findViewById(R.id.row6Strength);
         textView18.setText("" + six.level);
 
@@ -136,7 +136,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
         TextView textView29 = findViewById(R.id.row10MAC);
         textView29.setText("" + ten.BSSID + " / " + ten.frequency);
         TextView textView30 = findViewById(R.id.row10Strength);
-        textView30.setText("" + ten.level);
+        textView30.setText("" + ten.level);*/
 
 
 
