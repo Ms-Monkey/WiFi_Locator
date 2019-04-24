5cb5d300722df67f28f36c66426d8c996cd8fd69
Ms-Monkey <38368644+Ms-Monkey@users.noreply.github.com>
2019-04-24 13:10:52 +1200

    Displays signal strength, no need for string workarounds

--- DisplayMessageActivity.java
+++ DisplayMessageActivity.java
@@ -1,149 +1,327 @@
-package com.example.sorensjone.test;
+<?xml version="1.0" encoding="utf-8"?>
+<android.support.constraint.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:context=".DisplayMessageActivity"
+    tools:layout_editor_absoluteY="25dp">
 
-import android.content.Intent;
-import android.net.wifi.ScanResult;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TableLayout;
-import android.widget.TextView;
+    <TableLayout xmlns:android="http://schemas.android.com/apk/res/android"
+        android:id="@+id/myTable"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:layout_marginBottom="8dp"
+        android:layout_marginEnd="8dp"
+        android:layout_marginStart="8dp"
+        android:layout_marginTop="8dp"
+        android:stretchColumns="1"
+        android:background="#FFFFFF"
+        app:layout_constraintBottom_toBottomOf="parent"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent">
+        android:background="@drawable/background">
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
+        <TableRow>
+            android:background="#92C94A"
+            android:layout_width="fill_parent"
+            android:layout_height="wrap_content"
+            android:layout_margin="1dp"
 
-public class DisplayMessageActivity extends AppCompatActivity {
+            <TextView
+                android:id="@+id/textView1"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:padding="3dip"
+                android:text="Name of WiFi"/>
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+            <TextView
+                android:id="@+id/textView2"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"
+                android:text="MAC Address / Frequency"/>
 
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+            <TextView
+                android:id="@+id/textView3"
+                android:layout_width="50dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"
+                android:text="Signal Strength"/>
+        </TableRow>
 
-        ArrayList<WifiSignal> unorderedSignals = MainActivity.WifiOutput;
-        ArrayList<WifiSignal> signals = new ArrayList<WifiSignal>();
+        <TableRow>
+            android:background="#FFFFFF"
+            android:layout_width="fill_parent"
+            android:layout_height="wrap_content"
+            android:layout_margin="1dp"
 
-        // Capture the layout's TextView and set the string as its text
+            <TextView
+                android:id="@+id/row1Name"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:padding="3dip"/>
 
-        /*for(ScanResult temp: test) {
-            TextView textView = findViewById(R.id.row);
-            textView.setText(temp.SSID);
-        }*/
-        //for(int x = 0; x < myTable.getChildCount(); x++){
-        /*TableLayout table = (TableLayout) findViewById(R.id.myTable);
-        TextView textView = findViewById(R.id.row3MAC);
-        View row = table.getChildAt(0);
-        textView.setText("" + row);*/
-        //}
+            <TextView
+                android:id="@+id/row1MAC"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
 
+            <TextView
+                android:id="@+id/row1Strength"
+                android:layout_width="50dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+        </TableRow>
 
-        WifiSignal highest;
-        for (WifiSignal x: unorderedSignals){
-            highest = x;
-            if (signals.contains(x)){
-                continue;
-            }
+        <TableRow>
+            android:background="#FFFFFF"
+            android:layout_width="fill_parent"
+            android:layout_height="wrap_content"
+            android:layout_margin="1dp"
 
-            for (WifiSignal y: unorderedSignals){
-                //If the value is greater than y's and it isn't in signals already
-                if (highest.level < y.level && !signals.contains(y)){
-                    highest = y;
-                } else {
-                    continue;
-                }
-            }
-            signals.add(highest);
-        }
+            <TextView
+                android:id="@+id/row2Name"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:padding="3dip"/>
 
-        WifiSignal one = signals.get(0);
-        TextView textView1 = findViewById(R.id.row1Name);
-        textView1.setText(one.SSID);
-        TextView textView2 = findViewById(R.id.row1MAC);
-        textView2.setText(one.BSSID);
-        TextView textView3 = findViewById(R.id.row1Strength);
-        textView3.setText(one.distance);
+            <TextView
+                android:id="@+id/row2MAC"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
 
-        WifiSignal two = signals.get(1);
-        TextView textView4 = findViewById(R.id.row2Name);
-        textView4.setText(two.SSID);
-        TextView textView5 = findViewById(R.id.row2MAC);
-        textView5.setText(two.BSSID);
-        TextView textView6 = findViewById(R.id.row2Strength);
-        textView6.setText(two.distance);
+            <TextView
+                android:id="@+id/row2Strength"
+                android:layout_width="50dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+        </TableRow>
 
-        WifiSignal three = signals.get(2);
-        TextView textView7 = findViewById(R.id.row3Name);
-        textView7.setText(three.SSID);
-        TextView textView8 = findViewById(R.id.row3MAC);
-        textView8.setText(three.BSSID);
-        TextView textView9 = findViewById(R.id.row3Strength);
-        textView9.setText(three.distance);
+        <TableRow>
+            android:background="#FFFFFF"
+            android:layout_width="fill_parent"
+            android:layout_height="wrap_content"
+            android:layout_margin="1dp"
 
-        WifiSignal four = signals.get(3);
-        TextView textView10 = findViewById(R.id.row4Name);
-        textView10.setText(four.SSID);
-        TextView textView11 = findViewById(R.id.row4MAC);
-        textView11.setText(four.BSSID);
-        TextView textView12 = findViewById(R.id.row4Strength);
-        textView12.setText(four.distance);
+            <TextView
+                android:id="@+id/row3Name"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:padding="3dip"/>
 
-        WifiSignal five = signals.get(4);
-        TextView textView13 = findViewById(R.id.row5Name);
-        textView13.setText(five.SSID);
-        TextView textView14 = findViewById(R.id.row5MAC);
-        textView14.setText(five.BSSID);
-        TextView textView15 = findViewById(R.id.row5Strength);
-        textView15.setText(five.distance);
+            <TextView
+                android:id="@+id/row3MAC"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
 
-        /*WifiSignal six = signals.get(5);
-        TextView textView16 = findViewById(R.id.row6Name);
-        textView16.setText(six.SSID);
-        TextView textView17 = findViewById(R.id.row6MAC);
-        textView17.setText("" + six.BSSID + " / " + six.distance);
-        TextView textView18 = findViewById(R.id.row6Strength);
-        textView18.setText("" + six.level);
+            <TextView
+                android:id="@+id/row3Strength"
+                android:layout_width="50dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+        </TableRow>
 
-        WifiSignal seven = signals.get(6);
-        TextView textView19 = findViewById(R.id.row7Name);
-        textView19.setText(seven.SSID);
-        TextView textView20 = findViewById(R.id.row7MAC);
-        textView20.setText("" + seven.BSSID + " / " + seven.frequency);
-        TextView textView21 = findViewById(R.id.row7Strength);
-        textView21.setText("" + seven.level);
+        <TableRow>
+            android:background="#FFFFFF"
+            android:layout_width="fill_parent"
+            android:layout_height="wrap_content"
+            android:layout_margin="1dp"
 
-        WifiSignal eight = signals.get(7);
-        TextView textView22 = findViewById(R.id.row8Name);
-        textView22.setText(eight.SSID);
-        TextView textView23 = findViewById(R.id.row8MAC);
-        textView23.setText("" + eight.BSSID + " / " + eight.frequency);
-        TextView textView24 = findViewById(R.id.row8Strength);
-        textView24.setText("" + eight.level);
+            <TextView
+                android:id="@+id/row4Name"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:padding="3dip"/>
 
-        WifiSignal nine = signals.get(8);
-        TextView textView25 = findViewById(R.id.row9Name);
-        textView25.setText(nine.SSID);
-        TextView textView26 = findViewById(R.id.row9MAC);
-        textView26.setText("" + nine.BSSID + " / " + nine.frequency);
-        TextView textView27 = findViewById(R.id.row9Strength);
-        textView27.setText("" + nine.level);
+            <TextView
+                android:id="@+id/row4MAC"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
 
-        WifiSignal ten = signals.get(9);
-        TextView textView28 = findViewById(R.id.row10Name);
-        textView28.setText(ten.SSID);
-        TextView textView29 = findViewById(R.id.row10MAC);
-        textView29.setText("" + ten.BSSID + " / " + ten.frequency);
-        TextView textView30 = findViewById(R.id.row10Strength);
-        textView30.setText("" + ten.level);*/
+            <TextView
+                android:id="@+id/row4Strength"
+                android:layout_width="50dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+        </TableRow>
 
+        <TableRow>
+            android:background="#FFFFFF"
+            android:layout_width="fill_parent"
+            android:layout_height="wrap_content"
+            android:layout_margin="1dp"
 
+            <TextView
+                android:id="@+id/row5Name"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:padding="3dip"/>
 
-        //print length of scan results
-        //For each through the table rows
-        //Set each column in said row with applicable map vals
+            <TextView
+                android:id="@+id/row5MAC"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
 
-    }
+            <TextView
+                android:id="@+id/row5Strength"
+                android:layout_width="50dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+        </TableRow>
 
-}
+        <TableRow>
+            android:background="#FFFFFF"
+            android:layout_width="fill_parent"
+            android:layout_height="wrap_content"
+            android:layout_margin="1dp"
+
+            <TextView
+                android:id="@+id/row6Name"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:padding="3dip"/>
+
+            <TextView
+                android:id="@+id/row6MAC"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+
+            <TextView
+                android:id="@+id/row6Strength"
+                android:layout_width="50dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+        </TableRow>
+
+        <TableRow>
+            android:background="#FFFFFF"
+            android:layout_width="fill_parent"
+            android:layout_height="wrap_content"
+            android:layout_margin="1dp"
+
+            <TextView
+                android:id="@+id/row7Name"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:padding="3dip"/>
+
+            <TextView
+                android:id="@+id/row7MAC"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+
+            <TextView
+                android:id="@+id/row7Strength"
+                android:layout_width="50dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+        </TableRow>
+        <TableRow>
+            android:background="#FFFFFF"
+            android:layout_width="fill_parent"
+            android:layout_height="wrap_content"
+            android:layout_margin="1dp"
+
+            <TextView
+                android:id="@+id/row8Name"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:padding="3dip"/>
+
+            <TextView
+                android:id="@+id/row8MAC"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+
+            <TextView
+                android:id="@+id/row8Strength"
+                android:layout_width="50dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+        </TableRow>
+
+        <TableRow>
+            android:background="#FFFFFF"
+            android:layout_width="fill_parent"
+            android:layout_height="wrap_content"
+            android:layout_margin="1dp"
+
+            <TextView
+                android:id="@+id/row9Name"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:padding="3dip"/>
+
+            <TextView
+                android:id="@+id/row9MAC"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+
+            <TextView
+                android:id="@+id/row9Strength"
+                android:layout_width="50dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+        </TableRow>
+
+        <TableRow>
+            android:background="#FFFFFF"
+            android:layout_width="fill_parent"
+            android:layout_height="wrap_content"
+            android:layout_margin="1dp"
+
+            <TextView
+                android:id="@+id/row10Name"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:padding="3dip"/>
+
+            <TextView
+                android:id="@+id/row10MAC"
+                android:layout_width="100dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+
+            <TextView
+                android:id="@+id/row10Strength"
+                android:layout_width="50dp"
+                android:layout_height="25dp"
+                android:gravity="right"
+                android:padding="3dip"/>
+        </TableRow>
+
+    </TableLayout>
+
+</android.support.constraint.ConstraintLayout>
