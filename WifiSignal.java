8cc6c85dc6e2832fce860f5d0910a126a4779df1
Ms-Monkey <38368644+Ms-Monkey@users.noreply.github.com>
2019-04-27 15:06:58 +1200

    Added router object to each wifisignal if they match MAC

--- WifiSignal.java
+++ WifiSignal.java
@@ -13,21 +13,25 @@ import static android.support.v4.content.ContextCompat.getSystemService;
 
 public class WifiSignal{
     public String SSID, BSSID;
-    public int level, frequency, floor;
+    public int level, frequency, floor, distanceInt, x, y;
     public String distance;
+    Router router;
 
-    public WifiSignal(String name, String mac, int strength, int frequency, int floor) {
+    public WifiSignal(String name, String mac, int strength, int frequency, int floor, int x, int y, Router r) {
         this.SSID = name;
         this.BSSID = mac;
         this.level = strength;
         this.frequency = frequency;
         this.floor = floor;
+        this.x = x;
+        this.y = y;
+        this.router = router;
 
         //https://stackoverflow.com/questions/11217674/how-to-calculate-distance-from-wifi-router-using-signal-strength
         double exp = (27.55 - (20 * Math.log10(frequency)) + Math.abs(strength)) / 20.0;
         exp = Math.pow(10.0, exp);
-        int test = (int) exp;
-        this.distance = Integer.toString(test) + "m";
+        distanceInt = (int) exp;
+        this.distance = Integer.toString(distanceInt) + "m";
     }
 
 }
